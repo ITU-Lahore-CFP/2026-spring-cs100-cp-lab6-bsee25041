5a14236de914eb4f724084fc3433b6443f25ad06
bsee25041 <bsee25041@itu.edu.pk>
2026-03-29 20:54:00 +0500

    commit all task done

--- functions.cpp
+++ functions.cpp
@@ -3,10 +3,17 @@
 
 int sumOfSquares(int n) 
 {
+    if (n == 0)
+    return 0;
+  else
+  return n*n+sumOfSquares(n-1);
 }
 
 int findFactorial(int n)
 {
+    if(n == 0 || n == 1)
+    return 1;
+    return n * findFactorial(n - 1);
 }
 
 
--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 
 
 int main() {
+    int n;
+    cout <<"Enter a number:";
+    cin >> n;
+    int sum = sumOfSquares(n);
+    cout << "Sum of squares:"<< sum << endl;
+
+    
+    cout << "Enter a number:";
+    cin >> n;
+    cout <<"Factorial of the number is: " << findFactorial(n);
+    return 0;
    
 }
